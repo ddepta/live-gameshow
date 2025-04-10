8090a5fc709332e73a5984bb2257cc3639c4616d
Daniel Depta <56829289+ddepta@users.noreply.github.com>
2025-04-10 14:47:32 +0200

    hintergrund per button toggeln

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNG } from 'primeng/config';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { phosphorBinaryBold } from '@ng-icons/phosphor-icons/bold';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIcon],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [
+    provideIcons({
+      phosphorBinaryBold,
+    }),
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private primeng: PrimeNG) {}
   title = 'livestream-quiz';
+  gradientEnabled = false; // Default gradient state is now disabled
+
+  ngOnInit() {
+    // Initialize DOM with the gradient-disabled class
+    document.documentElement.classList.toggle(
+      'gradient-disabled',
+      !this.gradientEnabled
+    );
+  }
+
+  toggleGradient() {
+    this.gradientEnabled = !this.gradientEnabled;
+    document.documentElement.classList.toggle(
+      'gradient-disabled',
+      !this.gradientEnabled
+    );
+  }
 }
